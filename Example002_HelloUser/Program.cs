f05d7147e26a024f9197f89bc94375ebd9c298f6
Mikhail <kepler186f501@gmail.com>
2022-12-16 22:15:22 +0300

    updated 16-12-2022

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,4 +1,19 @@
-﻿Console.Write("Введите ваше имя ");
-string username = Console.ReadLine();
-Console.Write("Hello, ");
-Console.Write(username);
+﻿using System;
+class WriteAFewLines
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите Ваше любимое слово: ");
+        string favouriteWord = Console.ReadLine();
+        
+        Console.WriteLine("Сколько раз его напечатать? ");
+        int numberOfTimes = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < numberOfTimes; i++)
+        {
+            Console.WriteLine(favouriteWord);
+        }
+
+
+    }
+}
