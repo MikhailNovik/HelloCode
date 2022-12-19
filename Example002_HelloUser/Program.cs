beafd77bc0e16307dcbf521f76ed2540f04c0eda
Mikhail <kepler186f501@gmail.com>
2022-12-19 17:34:15 +0300

    add Lecture #2  materials

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -4,10 +4,10 @@ class WriteAFewLines
     static void Main()
     {
         Console.WriteLine("Введите Ваше любимое слово: ");
-        string favouriteWord = Console.ReadLine();
+        string favouriteWord = Console.ReadLine()!;
         
         Console.WriteLine("Сколько раз его напечатать? ");
-        int numberOfTimes = Convert.ToInt32(Console.ReadLine());
+        int numberOfTimes = Convert.ToInt32(Console.ReadLine()!);
 
         for (int i = 0; i < numberOfTimes; i++)
         {
---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,51 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index += 1;
+    }
+}
+
+void PrintArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.WriteLine(collection[index]);
+        index += 1;
+    }
+}
+
+int IndexOf(int[] collection, int findNum)
+{
+    int length = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < length)
+    {
+        if (collection[index] == findNum)
+        {
+             position = index;
+             break;
+        }
+        index += 1;
+    }   
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 4;
+array[6] = 4;
+
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+Console.WriteLine(pos);
